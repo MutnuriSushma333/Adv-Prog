0cb6162714f46685558f72d176495a6cb8ec8824
MutnuriSushma333 <54990183+MutnuriSushma333@users.noreply.github.com>
2020-05-28 13:49:16 +0530

    Add files via upload

---
+++ ass2.c
@@ -0,0 +1,15 @@
+/*print nth table*/
+/*While loop demo*/
+#include<stdio.h>
+main()
+{
+	int i,n;
+	printf("Enter the value of n:");
+	scanf("%d",&n);
+	i=1;
+	while(i<=10)
+	{
+		printf("\n %d X %d =%d",n,i,i*n);
+		i++;
+	}
+}
