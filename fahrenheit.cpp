f9df759bde6ca9084f2d2e2e1ec62d66e51b0dff
MutnuriSushma333 <54990183+MutnuriSushma333@users.noreply.github.com>
2020-07-08 16:15:39 +0530

    Add files via upload

---
+++ fahrenheit.cpp
@@ -0,0 +1,22 @@
+/*
+ * C++ Program to Perform Fahrenheit to Celsius Conversion
+ */
+ #include <iostream>
+ 
+ double fahrenheitToCelsius(double fahrenheit)
+ {
+     double celsius;
+ 
+     celsius = (fahrenheit - 32.0) * 5.0 / 9.0;
+     return celsius;
+ }
+ 
+ int main()
+ {
+     double fahrenheit;
+ 
+     std::cout << "Enter temperature in fahrenheit (in degrees) ";
+     std::cin  >> fahrenheit;
+     std::cout << "Temperature in Celsius (in degrees) = "
+               << fahrenheitToCelsius(fahrenheit) << std::endl;
+ }
